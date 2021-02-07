e019f81c53cf37b664b8402beba4ab06dbcf728c
Dhanuka99 <danuka1999lakshan@gmail.com>
2021-02-07 23:28:29 +0530

    PropertyFileConfiguration

--- src/util/FactoryConfiguration.java
+++ src/util/FactoryConfiguration.java
@@ -4,21 +4,25 @@ import entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.omg.CORBA.portable.InputStream;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private SessionFactory sessionFactory;
-    static  Properties properties = new Properties();
+
     public FactoryConfiguration() {
 
         try {
-            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("Config.properties"));
-            sessionFactory = new Configuration().mergeProperties(properties).buildSessionFactory();
 
-        } catch (IOException e) {
+            Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+            sessionFactory = configuration.buildSessionFactory();
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
