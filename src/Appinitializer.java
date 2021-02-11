50975ddea57a77aa2cce0a05b035bf2c313ac91c
Dhanuka99 <danuka1999lakshan@gmail.com>
2021-02-12 00:22:49 +0530

    Views Forms up to date

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -1,6 +1,11 @@
 import entity.Course;
 import entity.Register;
 import entity.Student;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
@@ -11,46 +16,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class Appinitializer {
+public class Appinitializer extends Application {
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream("src/hibernate.properties");
-        Properties props = new Properties();
-        props.load(fis);
+        launch(args);
+    }
 
-        Configuration configuration = new Configuration();
-        configuration.setProperties(props);
-       // configuration.addFile("student.hbm.xml");
-        Session session = FactoryConfiguration.getInstance().getSession();
-        Transaction transaction = session.beginTransaction();
-
-        Student student = new Student();
-        student.setStudentID("S001");
-        student.setStudentName("Dhanuka");
-        student.setStudentAddress("Kurunegala");
-        student.setStudentDOB("1999-21-21");
-        student.setStudentContact("0778090357");
-        student.setStudentGender("male");
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("view/DashboardForm.fxml"))));
+        primaryStage.show();
 
 
-        Course course = new Course();
-        course.setCourseCode("C001");
-        course.setCourseType("Degree level");
-        course.setCourseName("IT");
-        course.setDuration("4 years");
-
-        Register register = new Register();
-        register.setRegNo(1);
-        register.setRegFee(150000.00);
-        register.setRegDate("2020-1-3");
-
-        session.save(student);
-        session.save(course);
-        session.save(register);
-
-
-
-
-        transaction.commit();
-        session.close();
     }
 }
--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,4 +1,46 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class DashboardFormController {
+   // public Pane mainPanel;
+
+    @FXML
+    private Pane mainPanel;
+
+    public void initialize() throws IOException {
+      //  loadDefault();
+    }
+
+    public void studentOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+       // setUi("StudentForm");
+
+    }
+
+    public void courseOnMouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    public void registerOnMouseClicked(MouseEvent mouseEvent) throws IOException {
+      //  setUi("StudentRegiterForm");
+    }
+    private void loadDefault() throws IOException {
+    //  setUi("StudentRegiterForm");
+
+    }
+
+    private void setUi(String location) throws IOException {
+     /*   mainPanel.getChildren().clear();
+        mainPanel.getChildren().add(FXMLLoader.
+                load((Objects.requireNonNull(this.getClass().getClassLoader()
+                        .getResource("/view/" + location + ".fxml")))));*/
+    }
+
 }
